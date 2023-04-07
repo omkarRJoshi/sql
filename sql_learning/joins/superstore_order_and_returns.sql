3051cd4d7854b7ce5e5f2af3de65aefd7a85becc
omkarRJoshi <omkarjoshi9545@gmail.com>
2023-04-07 22:54:58 +0530

    solution for order and returns

--- sql_learning/joins/superstore_order_and_returns.sql
+++ sql_learning/joins/superstore_order_and_returns.sql
@@ -2,7 +2,8 @@ select * from superstore_orders limit 5;
 select * from returns limit 5;
 
 # 1. write a query to get region wise count of return orders
-select s.region, count(1) from 
+## corrected my solution by providing distinct to s.order_id
+select s.region, count(distinct s.order_id) from 
 returns as r
 inner join 
 superstore_orders as s 
@@ -11,6 +12,7 @@ group by s.region
 ;
 
 # 2. write a query to get category wise sales of orders that were not returned
+## approach 1 (my approach)
 select category, sum(sales) from(
 	select s.order_id as order_id, category, sales, r.order_id as returned_id from 
 	superstore_orders as s 
@@ -21,6 +23,14 @@ select category, sum(sales) from(
 where returned_id is null
 group by category
 ;
+
+## approach 2 (solution)
+select category,sum(o.sales) as total_sales
+from superstore_orders o
+left join returns r on o.order_id=r.order_id
+where r.order_id is null
+group by category;
+
 
 # 3. write a query to print sub categories 
 #	 where we have all 3 kinds of returns (others,bad quality,wrong items)
@@ -34,6 +44,7 @@ group by s.sub_category
 having count(distinct r.return_reason) = 3;
 
 # 4. write a query to find cities where not even a single order was returned.
+## my soltion - wrong ans
 select distinct(city) from(
 	select s.order_id as order_id, city, r.order_id as returned_id from 
 	superstore_orders as s 
@@ -41,7 +52,16 @@ select distinct(city) from(
 	returns as r
 	on s.order_id = r.order_id
 	) as t
-where returned_id is null;
+where returned_id is null
+order by city;
+
+## right ans
+select city
+from superstore_orders o
+left join returns r on o.order_id=r.order_id
+group by city
+having count(r.order_id)=0
+order by city;
 
 # 5. write a query to find top 3 subcategories by sales of returned orders in east region
 select s.Sub_Category, sum(s.sales) as total_sum
